7d18bcc7233279978e9396d7ee4cbc0d141bacbb
gul1994 <1may7777777@gmail.com>
2017-10-02 17:05:43 -0700

    initial check in

---
+++ src/ca/demo/salestool/Simple.java
@@ -0,0 +1,20 @@
+package ca.demo.salestool;
+
+public class Simple {	
+		String name="Mike";
+		int price=99;
+		int total=89;
+		
+		public Simple(String name){
+			System.out.println("MY constractor");
+		}
+		
+		public static void m1(String name){
+			System.out.println("My name is "+name);
+		}
+        public static void m2(int price){
+        	System.out.println("My price"+price);
+        }	
+}
+
+
---
+++ src/ca/demo/salestool/Source.java
@@ -0,0 +1,26 @@
+package ca.demo.salestool;
+
+
+
+public class Source {
+
+	public static void main(String[] args) {
+		String name;
+		name="Gulzina";
+		int price;
+		int pr;
+		pr=99;
+	    price=99;	
+	}
+	public void method1(String name){
+		System.out.println("My name is"+name);
+	}
+    public static void method2(int price){
+    	System.out.println("I have so many money"+price);
+   
+	
+    
+    	
+    }
+    }
+
