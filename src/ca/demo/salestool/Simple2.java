99ef8886253b05020eb2adf035c35304fd99a001
gul1994 <1may7777777@gmail.com>
2017-10-02 17:06:03 -0700

    local

---
+++ src/ca/demo/salestool/Simple2.java
@@ -0,0 +1,13 @@
+package ca.demo.salestool;
+
+public class Simple2 {
+
+	public static void main(String[] args) {
+		Simple.m1("Baighigit");
+		Simple.m2(99);
+        
+		Simple some=new Simple("Gulzina");
+		
+	}
+
+}
